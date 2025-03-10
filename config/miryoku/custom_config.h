93e20fa219006f21d21c9da49fdee97f39a588dd
Ilja Leiko <leikoilja@gmail.com>
2025-03-10 09:06:14 +0100

    fix: syntax

--- config/miryoku/custom_config.h
+++ config/miryoku/custom_config.h
@@ -1,6 +1,6 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
-#define MIRYOKU_ALPHAS QWERTY
-#define MIRYOKU_EXTRA COLEMAKDH
-#define MIRYOKU_NAV VI
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_EXTRA_COLEMAKDH
+#define MIRYOKU_NAV_VI
